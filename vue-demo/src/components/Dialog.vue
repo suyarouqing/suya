bfbd734f4f76edce29d78e837d5bb691e3381f82
suyarouqing <375602237@qq.com>
2017-09-25 12:12:20 +0800

    'deploy'

---
+++ vue-demo/src/components/Dialog.vue
@@ -0,0 +1,139 @@
+<template>	
+<div class="success" v-show="isShowDialog">			
+	<div class="success-box" >
+    <div class="success-font">
+      <div>
+        <img src="../assets/action_icon.png" alt="">
+        <img src="../assets/close_icon.png" alt="" @click="onCancel">
+      </div>
+      <div class="font-center">
+        <slot></slot>
+      </div>
+    </div>
+    <div class="button-position">
+      <button class="button cancel-button" @click="onCancel" v-show="isShowBtnCancel">
+       {{ cancelText }}
+      </button>
+      <button class="button success-button" @click="onSuccess" v-show="isShowBtnSuccess">
+        {{ okText }}
+      </button>
+    </div>
+	</div>
+</div>
+</template>
+<script>
+export default {
+  props: {
+    isShowBtnCancel: {
+      type: Boolean,
+      default: false
+    },
+    isShowDialog: {
+      type: Boolean,
+      default: false
+    },
+    isShowBtnSuccess: {
+      type: Boolean,
+      default: true
+    },
+    okText: {
+      type: String,
+      default: '确定'
+    },
+    cancelText: {
+      type: String,
+      default: '取消'
+    }
+  },
+  methods: {
+    onSuccess () {
+      this.$emit('on-success')
+    },
+    onCancel () {
+      this.$emit('on-close')
+    }
+  }
+}
+</script>
+<style>
+.success{
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.3);
+  box-sizing: border-box;
+  padding:140px 15px 0 15px;
+  position: fixed;
+  left:0;
+  top:0;
+  z-index: 999;
+}
+.success-box{
+	width:100%;
+  height:225px;
+  border-radius: 8px;
+	background-color:#ffffff;
+  position: relative;
+}
+.success-font{
+  box-sizing: border-box;
+  padding:18px 22px 0 22px;
+}
+.success-font > div{
+  width:100%;
+  height:17px;
+}
+.success-font > div img:first-child{
+  width:54px;
+  height:17px;
+  float:left;
+}
+.success-font > div img:last-child{
+  width:17px;
+  height:17px;
+  float:right
+}
+.success-font > div.font-center{
+  width:100%;
+  height:145px;
+  box-sizing: border-box;
+  padding:0 45px;
+  display:table-cell;
+  vertical-align:middle;
+}
+.success-font .font-center > p {
+  font-size:17px;
+  color:#333333;
+  text-align: center;
+  margin:auto;
+  display:inline-block;
+  vertical-align:middle;
+}
+.button-position{
+  width:100%;
+  height:44px;
+  position: absolute;
+  left:0;
+  bottom:0;
+  display: flex;
+}
+.button{
+  width:50%;
+  height:44px;
+  font-size: 17px;
+  color:#666666;
+  border:0 solid;
+  outline:0;
+  flex:1;
+}
+.success-button{
+  color: #fff;
+  background-color:#31bbff;
+  border-bottom-right-radius: 8px;
+}
+.cancel-button{
+  border-bottom-left-radius: 8px;
+  background-color:#ffffff;
+  border-top:1px solid #dcdcdc;
+  border-right:1px solid #dcdcdc;
+}
+</style>
--- vue-demo/src/components/Hello.vue
+++ vue-demo/src/components/Hello.vue
@@ -2,21 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <button @click="isShowDialog = true">按钮</button>
+    <dialog-box :isShowDialog="isShowDialog" @on-success="onSuccess" @on-close="onCancel" :isShowBtnSuccess="isShowBtnSuccess" :isShowBtnCancel="isShowBtnCancel">
+      <p>{{promptMsg}}</p>
+      <p>{{promptMsgs}}</p>
+    </dialog-box>
   </div>
 </template>
 
@@ -25,7 +15,20 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      isShowDialog: false,
+      isShowBtnSuccess: true,
+      isShowBtnCancel: true,
+      promptMsg: '会务已经开始准备',
+      promptMsgs: '现在取消同样会有会务费'
+    }
+  },
+  methods: {
+    onSuccess () {
+      this.isShowDialog = false
+    },
+    onCancel () {
+      this.isShowDialog = false
     }
   }
 }
@@ -33,21 +36,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
